43b17e747761c8aa75b008b877253a1cb07d35d0
Neetu-12 <93900988+Neetu-12@users.noreply.github.com>
2021-11-08 15:31:38 +0530

    duplicate in func.py
    duplicate in func.py

---
+++ duplicate in func.py
@@ -0,0 +1,21 @@
+a=["Delhi", "Delhi", "Mumbai", "Mumbai", "Delhi", "Chennai", 'Chennai']
+i=0
+r=[]
+c=0
+while i<len(a):
+    if a[i] not in r:
+        c=c+1
+        r.append(a[i])
+    i=i+1
+print(r)
+
+
+
+# ##q=2
+
+x=1
+y=2
+x, y, z = x, x, y
+z, y, z = x, y, z
+print(x,y,z)
+
